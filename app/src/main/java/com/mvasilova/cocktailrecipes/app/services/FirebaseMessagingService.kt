537549c2d83bd175ae3bdecfe0b519cba7383c2e
Marina <marinochka12vasilova@gmail.com>
2020-04-24 14:00:00 +0300

    add google-services (analytics, fcm, crashlytics)

---
+++ app/src/main/java/com/mvasilova/cocktailrecipes/app/services/FirebaseMessagingService.kt
@@ -0,0 +1,60 @@
+package com.mvasilova.cocktailrecipes.app.services
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.mvasilova.cocktailrecipes.R
+import com.mvasilova.cocktailrecipes.presentation.MainActivity
+
+class FirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        sendNotification(remoteMessage.notification?.title, remoteMessage.notification?.body)
+    }
+
+    private fun sendNotification(title: String?, message: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_drinks)
+            .setColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            .setContentTitle(title)
+            .setContentText(message)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                getString(R.string.default_notification_name_channel),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    companion object {
+        const val FIREBASE_TAG = "FirebaseMsgService"
+    }
+}
